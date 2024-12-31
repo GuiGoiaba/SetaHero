5a5cd5485541458d68f0d4fb632aa98ca801bf6d
Guilherme R <bananilsondasilva@gmail.com>
2024-12-30 22:38:55 -0300

    tbm nao sei

--- Main.c
+++ Main.c
@@ -1,24 +1,26 @@
 #include "objetos.h"
 
 // GLOBAIS
-// Dimensões da tela
 #define LARGURA 800
 #define ALTURA 600
 #define FPS 60
 #define MAX_NOME 20
 #define TAM_SETA 50
 #define VELOCIDADE 0.5f
+#define MAX_INIMIGOS 2
 
 // CONFIGURANDO A SETA
-typedef struct seta{
+typedef struct seta
+{
     int x, y;
     int direcao;
 } Seta;
 
-int main(){
+int main()
+{
     // Inicializa Allegro
     inicializa_allegro();
-    
+
     // Criação da tela
     ALLEGRO_DISPLAY *janela = cria_tela();
 
@@ -26,43 +28,55 @@ int main(){
     ALLEGRO_BITMAP *seta_cima, *seta_baixo, *seta_direita, *seta_esquerda = NULL;
     ALLEGRO_EVENT_QUEUE *fila_eventos = al_create_event_queue();
     ALLEGRO_TIMER *timer = al_create_timer(1.0 / FPS);
-    
+
     // Inicializando seta
-    Seta seta = criar_seta(LARGURA, ALTURA / 2 - TAM_SETA / 2);
+    Seta setas[MAX_INIMIGOS];
+    int espaco_inicial = 150;
+    for (int i = 0; i < MAX_INIMIGOS; i++)
+    {
+        setas[i] = criar_seta(LARGURA + i * espaco_inicial, ALTURA / 2 - TAM_SETA / 2);
+    }
     inicializa_recursos(&seta_cima, &seta_baixo, &seta_direita, &seta_esquerda);
     int seta_jogador = 0;
     int pontuacao = 0;
     int seg = 0;
     int vidas = 3;
-    int ult_pont = 100000000;
+    int ult_pont = 0;
     float vel_inim = 1.0f;
     bool fechar = false;
     bool redraw = true;
-    
+
     // Configuração de eventos
     configura_eventos(fila_eventos, janela, timer);
 
     // Menu principal
-    int menu_option = 0; 
+    int menu_option = 0;
     bool dentro_menu = true;
-    char nome_jogador[MAX_NOME] = "guilhermeee";
+    char nome_jogador[MAX_NOME] = "";
 
     srand(time(NULL));
     al_start_timer(timer);
 
-    while (!fechar){
+    while (!fechar)
+    {
         ALLEGRO_EVENT ev;
         al_wait_for_event(fila_eventos, &ev);
-        if (dentro_menu){
-            if (ev.type == ALLEGRO_EVENT_KEY_DOWN){
-                if (ev.keyboard.keycode == ALLEGRO_KEY_DOWN){
+        if (dentro_menu)
+        {
+            if (ev.type == ALLEGRO_EVENT_KEY_DOWN)
+            {
+                if (ev.keyboard.keycode == ALLEGRO_KEY_DOWN)
+                {
                     menu_option = (menu_option + 1) % 2;
                 }
-                else if (ev.keyboard.keycode == ALLEGRO_KEY_UP){
+                else if (ev.keyboard.keycode == ALLEGRO_KEY_UP)
+                {
                     menu_option = (menu_option - 1 + 2) % 2;
                 }
-                else if (ev.keyboard.keycode == ALLEGRO_KEY_ENTER){
-                    if (menu_option == 0){
+                else if (ev.keyboard.keycode == ALLEGRO_KEY_ENTER)
+                {
+                    if (menu_option == 0)
+                    {
                         dentro_menu = false;
                         insere_nome(nome_jogador);
                         dentro_menu = false;
@@ -70,28 +84,33 @@ int main(){
                         vidas = 3;
                         vel_inim = 1.0f;
                     }
-                    else{
+                    else
+                    {
                         fechar = true;
                     }
                 }
             }
             desenha_menu(menu_option, nome_jogador, ult_pont);
         }
-        else{
-            if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE){
+        else
+        {
+            if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+            {
                 fechar = true;
             }
-            else if (ev.type == ALLEGRO_EVENT_TIMER){
+            else if (ev.type == ALLEGRO_EVENT_TIMER)
+            {
+                dentro_menu = false;
                 redraw = true;
 
-                seta.x -= (int)vel_inim;
+                for (int i = 0; i < MAX_INIMIGOS; i++)
+                {
+                    setas[i].x -= (int)vel_inim;
 
-                if (seta.x + TAM_SETA < 0){
-                    vidas--;
-                    seta = criar_seta(LARGURA, ALTURA / 2 - TAM_SETA / 2);
-                    if (seta.x < LARGURA / 2 - TAM_SETA / 2 &&
-                            seta.x + TAM_SETA > LARGURA / 2 + TAM_SETA / 2){
+                    if (setas[i].x + TAM_SETA < 0)
+                    {
                         vidas--;
+                        setas[i] = criar_seta(LARGURA, ALTURA / 2 - TAM_SETA / 2);
                     }
                 }
 
@@ -104,55 +123,67 @@ int main(){
                         fechar = true;
                 }
             }
-            else if (ev.type == ALLEGRO_EVENT_KEY_DOWN){
-                switch (ev.keyboard.keycode){
-                    case (ALLEGRO_KEY_ESCAPE):
-                        fechar = true;
-                        break;
-                    case (ALLEGRO_KEY_UP):
-                        seta_jogador = 0;
-                        break;
-                    case (ALLEGRO_KEY_DOWN):
-                        seta_jogador = 1;
-                        break;
-                    case (ALLEGRO_KEY_RIGHT):
-                        seta_jogador = 2;
-                        break;
-                    case (ALLEGRO_KEY_LEFT):
-                        seta_jogador = 3;
-                        break;
-            }
-                if (seta.x < LARGURA / 2 + TAM_SETA / 2 &&
-                    seta.x > LARGURA / 2 - TAM_SETA / 2 &&
-                    seta.direcao == seta_jogador)
+            else if (ev.type == ALLEGRO_EVENT_KEY_DOWN)
+            {
+                switch (ev.keyboard.keycode)
+                {
+                case (ALLEGRO_KEY_ESCAPE):
+                    fechar = true;
+                    break;
+                case (ALLEGRO_KEY_UP):
+                    seta_jogador = 0;
+                    break;
+                case (ALLEGRO_KEY_DOWN):
+                    seta_jogador = 1;
+                    break;
+                case (ALLEGRO_KEY_RIGHT):
+                    seta_jogador = 2;
+                    break;
+                case (ALLEGRO_KEY_LEFT):
+                    seta_jogador = 3;
+                    break;
+                }
+                for (int i = 0; i < MAX_INIMIGOS; i++)
+                {
+                    if (setas[i].x < LARGURA / 2 + TAM_SETA / 2 &&
+                        setas[i].x > LARGURA / 2 - TAM_SETA / 2 &&
+                        setas[i].direcao == seta_jogador)
                     {
-                    pontuacao += 50;
-                    seta = criar_seta(LARGURA, ALTURA / 2 - TAM_SETA / 2);
-                }
-                else if (seta.x < LARGURA / 2 + TAM_SETA / 2 &&
-                        seta.x > LARGURA / 2 - TAM_SETA / 2 &&
-                        seta.direcao != seta_jogador)
-                        {
-                    vidas--;
-                    seta = criar_seta(LARGURA, ALTURA / 2 - TAM_SETA / 2);
+                        pontuacao += 50;
+                        setas[i] = criar_seta(LARGURA, ALTURA / 2 - TAM_SETA / 2);
+                    }
+                    else if (setas[i].x < LARGURA / 2 + TAM_SETA / 2 &&
+                             setas[i].x > LARGURA / 2 - TAM_SETA / 2 &&
+                             setas[i].direcao != seta_jogador)
+                    {
+                        vidas--;
+                        setas[i] = criar_seta(LARGURA, ALTURA / 2 - TAM_SETA / 2);
+                    }
                 }
             }
-            if (vidas <= 0){
+            if (vidas <= 0)
+            {
                 menu_final(pontuacao, nome_jogador);
                 ult_pont = pontuacao;
-                while (1){
+                while (1)
+                {
                     ALLEGRO_EVENT ev2;
                     al_wait_for_event(fila_eventos, &ev2);
-                    if (ev2.type == ALLEGRO_EVENT_KEY_DOWN && ev2.keyboard.keycode == ALLEGRO_KEY_ENTER){
+                    if (ev2.type == ALLEGRO_EVENT_KEY_DOWN && ev2.keyboard.keycode == ALLEGRO_KEY_ENTER)
+                    {
                         dentro_menu = true;
                         break;
                     }
                 }
             }
-            if (redraw && al_is_event_queue_empty(fila_eventos)){
+            if (redraw && al_is_event_queue_empty(fila_eventos))
+            {
                 redraw = false;
 
-                desenha_seta(seta, seta_cima, seta_baixo, seta_direita, seta_esquerda);
+                for (int i = 0; i < MAX_INIMIGOS; i++)
+                {
+                    desenha_seta(setas[i], seta_cima, seta_baixo, seta_direita, seta_esquerda);
+                }
 
                 Seta player = {LARGURA / 2 - TAM_SETA / 2, ALTURA / 2 - TAM_SETA / 2, seta_jogador};
                 desenha_seta(player, seta_cima, seta_baixo, seta_direita, seta_esquerda);
--- objetos.c
+++ objetos.c
@@ -5,21 +5,25 @@
 #define MAX_NOME 20
 #define TAM_SETA 50
 
-typedef struct seta{
+typedef struct seta
+{
     int x, y;
-    int type;
+    int direcao;
 } Seta;
 
-Seta criar_seta(int x, int y){
+Seta criar_seta(int x, int y)
+{
     Seta seta;
     seta.x = x;
     seta.y = y;
-    seta.type = rand() % 4;
+    seta.direcao = rand() % 4;
     return seta;
 }
 
-void desenha_seta(Seta seta, ALLEGRO_BITMAP *seta_cima, ALLEGRO_BITMAP *seta_baixo, ALLEGRO_BITMAP *seta_direita, ALLEGRO_BITMAP *seta_esquerda){
-    switch (seta.type){
+void desenha_seta(Seta seta, ALLEGRO_BITMAP *seta_cima, ALLEGRO_BITMAP *seta_baixo, ALLEGRO_BITMAP *seta_direita, ALLEGRO_BITMAP *seta_esquerda)
+{
+    switch (seta.direcao)
+    {
     case 0: // Cima
         al_draw_bitmap(seta_cima, seta.x, seta.y, 0);
         break;
@@ -35,13 +39,15 @@ void desenha_seta(Seta seta, ALLEGRO_BITMAP *seta_cima, ALLEGRO_BITMAP *seta_bai
     }
 }
 
-void desenha_menu(int escolha, const char *nome_jogador, int ult_pont){
+void desenha_menu(int escolha, const char *nome_jogador, int ult_pont)
+{
     ALLEGRO_FONT *fonte = al_create_builtin_font();
     al_clear_to_color(al_map_rgb(192, 192, 192));
 
     al_draw_text(fonte, al_map_rgb(0, 0, 0), LARGURA / 2, ALTURA / 4, ALLEGRO_ALIGN_CENTRE, "SETA HERO");
 
-    if (nome_jogador[0] != '\0'){
+    if (nome_jogador[0] != '\0')
+    {
         al_draw_textf(fonte, al_map_rgb(0, 0, 0), LARGURA / 2, ALTURA / 4 + 40, ALLEGRO_ALIGN_CENTRE,
                       "Jogador: %s", nome_jogador);
         al_draw_textf(fonte, al_map_rgb(0, 0, 0), LARGURA / 2, ALTURA / 4 + 80, ALLEGRO_ALIGN_CENTRE,
@@ -49,18 +55,22 @@ void desenha_menu(int escolha, const char *nome_jogador, int ult_pont){
     }
 
     const char *opcoes[] = {"Iniciar Jogo", "Sair"};
-    for (int i = 0; i < 2; i++){
-        if (i == escolha){
+    for (int i = 0; i < 2; i++)
+    {
+        if (i == escolha)
+        {
             al_draw_text(fonte, al_map_rgb(0, 0, 0), LARGURA / 2, ALTURA / 2 + i * 40, ALLEGRO_ALIGN_CENTRE, opcoes[i]);
         }
-        else{
+        else
+        {
             al_draw_text(fonte, al_map_rgb(128, 128, 128), LARGURA / 2, ALTURA / 2 + i * 40, ALLEGRO_ALIGN_CENTRE, opcoes[i]);
         }
     }
     al_flip_display();
 }
 
-void insere_nome(char *nome_jogador){
+void insere_nome(char *nome_jogador)
+{
     ALLEGRO_FONT *fonte = al_create_builtin_font();
     al_clear_to_color(al_map_rgb(192, 192, 192));
 
@@ -73,21 +83,27 @@ void insere_nome(char *nome_jogador){
     int inicio = 0;
     bool cabou = false;
 
-    while (!cabou){
+    while (!cabou)
+    {
         ALLEGRO_EVENT ev;
         al_wait_for_event(input_teclado, &ev);
-        if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE){
+        if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+        {
             cabou = true;
         }
-        if (ev.type == ALLEGRO_EVENT_KEY_CHAR){
-            if (ev.keyboard.unichar >= 32 && ev.keyboard.unichar <= 126 && inicio < MAX_NOME - 1){
+        if (ev.type == ALLEGRO_EVENT_KEY_CHAR)
+        {
+            if (ev.keyboard.unichar >= 32 && ev.keyboard.unichar <= 126 && inicio < MAX_NOME - 1)
+            {
                 nome_jogador[inicio++] = (char)ev.keyboard.unichar;
                 nome_jogador[inicio] = '\0';
             }
-            else if (ev.keyboard.keycode == ALLEGRO_KEY_BACKSPACE && inicio > 0){
+            else if (ev.keyboard.keycode == ALLEGRO_KEY_BACKSPACE && inicio > 0)
+            {
                 nome_jogador[--inicio] = '\0';
             }
-            else if (ev.keyboard.keycode == ALLEGRO_KEY_ENTER){
+            else if (ev.keyboard.keycode == ALLEGRO_KEY_ENTER)
+            {
                 cabou = true;
             }
 
@@ -100,7 +116,8 @@ void insere_nome(char *nome_jogador){
     al_destroy_event_queue(input_teclado);
 }
 
-void menu_final(int pontuacao, char *nome_jogador){
+void menu_final(int pontuacao, char *nome_jogador)
+{
     ALLEGRO_FONT *fonte = al_create_builtin_font();
     al_clear_to_color(al_map_rgb(192, 192, 192));
 
@@ -112,8 +129,10 @@ void menu_final(int pontuacao, char *nome_jogador){
     al_flip_display();
 }
 
-void inicializa_allegro() {
-    if (!al_init()) {
+void inicializa_allegro()
+{
+    if (!al_init())
+    {
         printf("Erro ao inicializar o Allegro.\n");
         exit(EXIT_FAILURE);
     }
@@ -124,79 +143,39 @@ void inicializa_allegro() {
     al_init_ttf_addon();
 }
 
-ALLEGRO_DISPLAY* cria_tela() {
+ALLEGRO_DISPLAY *cria_tela()
+{
     ALLEGRO_DISPLAY *janela = al_create_display(LARGURA, ALTURA);
-    if (!janela) {
+    if (!janela)
+    {
         printf("Erro ao criar a tela.\n");
         exit(EXIT_FAILURE);
     }
     return janela;
 }
 
-void inicializa_recursos(ALLEGRO_BITMAP **seta_cima, ALLEGRO_BITMAP **seta_baixo, ALLEGRO_BITMAP **seta_direita, ALLEGRO_BITMAP **seta_esquerda) {
+void inicializa_recursos(ALLEGRO_BITMAP **seta_cima, ALLEGRO_BITMAP **seta_baixo, ALLEGRO_BITMAP **seta_direita, ALLEGRO_BITMAP **seta_esquerda)
+{
     *seta_cima = al_load_bitmap("Assets/seta_cima.png");
     *seta_baixo = al_load_bitmap("Assets/seta_baixo.png");
     *seta_direita = al_load_bitmap("Assets/seta_direita.png");
     *seta_esquerda = al_load_bitmap("Assets/seta_esquerda.png");
-    if (!*seta_cima || !*seta_baixo || !*seta_direita || !*seta_esquerda) {
+    if (!*seta_cima || !*seta_baixo || !*seta_direita || !*seta_esquerda)
+    {
         printf("Erro ao carregar os bitmaps.\n");
         exit(EXIT_FAILURE);
     }
 }
 
-void configura_eventos(ALLEGRO_EVENT_QUEUE *fila_eventos, ALLEGRO_DISPLAY *janela, ALLEGRO_TIMER *timer) {
+void configura_eventos(ALLEGRO_EVENT_QUEUE *fila_eventos, ALLEGRO_DISPLAY *janela, ALLEGRO_TIMER *timer)
+{
     al_register_event_source(fila_eventos, al_get_display_event_source(janela));
     al_register_event_source(fila_eventos, al_get_timer_event_source(timer));
     al_register_event_source(fila_eventos, al_get_keyboard_event_source());
 }
 
-void menu_principal(ALLEGRO_EVENT_QUEUE *fila_eventos, int *menu_option, bool *dentro_menu, char *nome_jogador, int *pontuacao, int *vidas, float *vel_inim, bool *fechar, int ult_pont) {
-    ALLEGRO_EVENT ev;
-    al_wait_for_event(fila_eventos, &ev);
-    if (ev.type == ALLEGRO_EVENT_KEY_DOWN) {
-        if (ev.keyboard.keycode == ALLEGRO_KEY_DOWN) {
-            *menu_option = (*menu_option + 1) % 2;
-        } else if (ev.keyboard.keycode == ALLEGRO_KEY_UP) {
-            *menu_option = (*menu_option - 1 + 2) % 2;
-        } else if (ev.keyboard.keycode == ALLEGRO_KEY_ENTER) {
-            if (*menu_option == 0) {
-                *dentro_menu = false;
-                insere_nome(nome_jogador);
-                *pontuacao = 0;
-                *vidas = 3;
-                *vel_inim = 1.0f;
-            } else {
-                *fechar = true;
-            }
-        }
-    }
-    desenha_menu(*menu_option, nome_jogador, ult_pont);
-}
-
-void jogo(Seta *seta, int *pontuacao, int *vidas, float *vel_inim, bool *fechar, bool *redraw, ALLEGRO_BITMAP *seta_cima, ALLEGRO_BITMAP *seta_baixo, ALLEGRO_BITMAP *seta_direita, ALLEGRO_BITMAP *seta_esquerda, ALLEGRO_EVENT_QUEUE *fila_eventos, int seta_jogador) {
-    ALLEGRO_EVENT ev;
-    al_wait_for_event(fila_eventos, &ev);
-    if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
-        *fechar = true;
-    } else if (ev.type == ALLEGRO_EVENT_TIMER) {
-        *redraw = true;
-        seta->x -= (int)*vel_inim;
-        if (seta->x + TAM_SETA < 0) {
-            (*vidas)--;
-            *seta = criar_seta(LARGURA, ALTURA / 2 - TAM_SETA / 2);
-        }
-    }
-
-    if (*redraw && al_is_event_queue_empty(fila_eventos)) {
-        *redraw = false;
-        desenha_seta(*seta, seta_cima, seta_baixo, seta_direita, seta_esquerda);
-        Seta player = {LARGURA / 2 - TAM_SETA / 2, ALTURA / 2 - TAM_SETA / 2, seta_jogador};
-        desenha_seta(player, seta_cima, seta_baixo, seta_direita, seta_esquerda);
-        al_flip_display();
-    }
-}
-
-void finaliza_recursos(ALLEGRO_DISPLAY *janela, ALLEGRO_EVENT_QUEUE *fila_eventos, ALLEGRO_TIMER *timer, ALLEGRO_BITMAP *seta_cima, ALLEGRO_BITMAP *seta_baixo, ALLEGRO_BITMAP *seta_direita, ALLEGRO_BITMAP *seta_esquerda) {
+void finaliza_recursos(ALLEGRO_DISPLAY *janela, ALLEGRO_EVENT_QUEUE *fila_eventos, ALLEGRO_TIMER *timer, ALLEGRO_BITMAP *seta_cima, ALLEGRO_BITMAP *seta_baixo, ALLEGRO_BITMAP *seta_direita, ALLEGRO_BITMAP *seta_esquerda)
+{
     al_destroy_display(janela);
     al_destroy_event_queue(fila_eventos);
     al_destroy_timer(timer);
